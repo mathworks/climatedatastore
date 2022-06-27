6b3f0a9c275b8a0b5f31eb95f9cb4161a270fac6
Rob Purser <rpurser@mathworks.com>
2022-06-27 07:57:11 -0400

    Revise testToolbox utility to be compatible with R2020a

--- buildutil/testToolbox.m
+++ buildutil/testToolbox.m
@@ -1,55 +1,63 @@
-function testToolbox(connectToServer, showReports)
-%RUNTESTWITHCODECOVERAGE Summary of this function goes here
-%   Detailed explanation goes here
-
+function testToolbox(connectToServer, htmlReports)
+    %RUNTESTWITHCODECOVERAGE Summary of this function goes here
+    %   Detailed explanation goes here
+    
     arguments
         connectToServer (1,1) logical = false;
-        showReports (1,1) logical = false;
+        htmlReports (1,1) logical = false;
     end
     
-    import matlab.unittest.plugins.codecoverage.CoverageReport
-    import matlab.unittest.plugins.codecoverage.CoberturaFormat
+    import matlab.unittest.TestSuite;
+    import matlab.unittest.TestRunner;
+    import matlab.unittest.Verbosity;
+    import matlab.unittest.plugins.CodeCoveragePlugin;
+    import matlab.unittest.plugins.XMLPlugin;
+    import matlab.unittest.plugins.codecoverage.CoberturaFormat;
+    import matlab.unittest.selectors.HasTag;
     
-        outputDirectory = "report";
-        if isempty(dir(outputDirectory))
-            mkdir(outputDirectory)
-        end
-        import matlab.unittest.plugins.CodeCoveragePlugin
-        P = matlab.unittest.parameters.Parameter.fromData('useMock', struct('value', ~connectToServer));
-        suite = matlab.unittest.TestSuite.fromClass(?smokeTest,'ExternalParameters',P);
-        runner = testrunner("textoutput");
+    oldpath  = addpath("test",genpath("climatedatastoreToolbox"));
+    finalize = onCleanup(@()(path(oldpath)));
+
+    outputDirectory = "report";
+    if isempty(dir(outputDirectory))
+        mkdir(outputDirectory)
+    end
     
-        if showReports
-            htmlReport = CoverageReport(outputDirectory,MainFile = "codecoverage.html");
-            p = CodeCoveragePlugin.forFolder("climatedatastoreToolbox","Producing",htmlReport);
-            runner.addPlugin(p)
+    P = matlab.unittest.parameters.Parameter.fromData('useMock', struct('value', ~connectToServer));
+    suite = TestSuite.fromClass(?smokeTest,'ExternalParameters',P);
+    if ~connectToServer
+        suite = suite.selectIf(HasTag('SupportsMock'));
+    end
+    
+    runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
+    
+    if htmlReports
+        htmlReport = CoverageReport(outputDirectory,MainFile = "codecoverage.html");
+        p = CodeCoveragePlugin.forFolder("climatedatastoreToolbox","Producing",htmlReport);
+        runner.addPlugin(p)
+    else
+        runner.addPlugin(XMLPlugin.producingJUnitFormat('report/test-results.xml'));
+        runner.addPlugin(CodeCoveragePlugin.forFolder({'climatedatastoreToolbox'}, 'IncludingSubfolders', false, 'Producing', CoberturaFormat('report/codecoverage.xml')));
+    end
+    
+    results = runner.run(suite);
+    results.generateHTMLReport(outputDirectory,MainFile = "testreport.html");
+    
+    if ~htmlReports
+        % Generate the JSON files for the shields in the readme.md
+        codecovInfo = readstruct(fullfile(outputDirectory,"codecoverage.xml"));
+        codeCoverage = round(codecovInfo.line_rateAttribute * 100,1);
+        if codeCoverage > 95
+            color = "green";
+        elseif codeCoverage > 85
+            color = "orange";
+        elseif codeCoverage > 75
+            color = "yellow";
         else
-            xmlReport = CoberturaFormat(fullfile(outputDirectory,"codecoverage.xml"));
-            p = CodeCoveragePlugin.forFolder("climatedatastoreToolbox","Producing",xmlReport);
-            runner.addPlugin(p)
+            color = "red";
         end
+        %writeBadgeJSONFile("code coverage",codeCoverage + "%", color)
+    end
     
-        results = runner.run(suite);
-        results.generateHTMLReport(outputDirectory,MainFile = "testreport.html");
-    
-        if showReports
-            web(fullfile(outputDirectory,"testreport.html"))
-            web(fullfile(outputDirectory,"codecoverage.html"))
-        else
-            % Generate the JSON files for the shields in the readme.md
-            codecovInfo = readstruct(fullfile(outputDirectory,"codecoverage.xml"));
-            codeCoverage = round(codecovInfo.line_rateAttribute * 100,1);
-            if codeCoverage > 95
-                color = "green";
-            elseif codeCoverage > 85
-                color = "orange";
-            elseif codeCoverage > 75
-                color = "yellow";
-            else
-                color = "red";
-            end
-            writeBadgeJSONFile("code coverage",codeCoverage + "%", color)
-        end
-        
-        results.assertSuccess()
+    results.assertSuccess()
 end
