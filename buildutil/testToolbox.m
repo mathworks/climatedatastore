756ac16004724ef9a6a6f7f12ac5ba340248cf7e
Rob Purser <rpurser@mathworks.com>
2022-06-27 10:36:34 -0400

    readstruct not supported in R2020a

--- buildutil/testToolbox.m
+++ buildutil/testToolbox.m
@@ -49,18 +49,29 @@ function testToolbox(connectToServer, htmlReports)
     
     if ~htmlReports
         % Generate the JSON files for the shields in the readme.md
-        codecovInfo = readstruct(fullfile(outputDirectory,"codecoverage.xml"));
-        codeCoverage = round(codecovInfo.line_rateAttribute * 100,1);
-        if codeCoverage > 95
-            color = "green";
-        elseif codeCoverage > 85
-            color = "orange";
-        elseif codeCoverage > 75
-            color = "yellow";
+
+        codecoverageFileName = fullfile(outputDirectory,"codecoverage.xml");
+        if verLessThan('matlab','9.8')
+            % R2020a compatible
+            xmlDOM = parseFile(matlab.io.xml.dom.Parser,codecoverageFileName);
+            codeCoverageRate = str2double(xmlDOM.Children.getAttribute("line-rate"));
         else
-            color = "red";
+            % readstruct was introduced in R2020b / version 9.8
+            codecovInfo = readstruct(codecoverageFileName);
+            codeCoverageRate = codecovInfo.line_rateAttribute;
         end
-        %writeBadgeJSONFile("code coverage",codeCoverage + "%", color)
+        
+%         codeCoverageRate = round(codeCoverageRate * 100,1);
+%         if codeCoverageRate > 95
+%             color = "green";
+%         elseif codeCoverageRate > 85
+%             color = "orange";
+%         elseif codeCoverageRate > 75
+%             color = "yellow";
+%         else
+%             color = "red";
+%         end
+        %writeBadgeJSONFile("code coverage",codeCoverageRate + "%", color)
     end
     
     results.assertSuccess()
