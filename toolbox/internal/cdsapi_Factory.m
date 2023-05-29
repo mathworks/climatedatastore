b988323b7226582345be03bf009b46241358f84c
Rob Purser <rpurser@mathworks.com>
2023-05-29 07:56:08 -0400

    Fixed Issue #14.  Hid test infrastructure from dependency analyzer.

--- toolbox/internal/cdsapi_Factory.m
+++ toolbox/internal/cdsapi_Factory.m
@@ -19,7 +19,11 @@ classdef (Sealed, Hidden) cdsapi_Factory < handle
         function cdsapiClient = getCdsapi(options)
             theFactory = cdsapi_Factory.getInstance();
             if theFactory.UseMocks
-                cdsapiClient = cdsapi_ClientMock(options);
+                % Do not directly call the constructor of the mock object that is ONLY used by automated test system.
+                % That way, dependency analysis does not try to include the test infrastructure as part of the toolbox.
+                % Note if we just do eval("cdsapi_ClientMock(options)"), the dependency system is smart enough to resolve that!
+                mockName = "cdsapi_ClientMock(options)";
+                cdsapiClient = eval(mockName); %#ok<EVLCS> 
             else
                 cdsapiClient = cdsapi_Client(options);
             end
